ef88c36b7913bc5533fdf3506181202c8ab1ad16
Johnfred Cornell <johnfred.cornell.287@my.csun.edu>
2018-02-24 14:07:48 -0800

    added queryIndcator script and button

---
+++ Assets/Scripts/QueryIndicator.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class QueryIndicator : MonoBehaviour {
+
+    // this is the UI.Text or other UI element you want to toggle
+    public MaskableGraphic ImageToToggle, TextToToggle;
+
+    public float Interval = 1f;
+    public float StartDelay = 0.5f;
+    public bool CurrentState = true;
+    public bool DefaultState = true;
+    bool _isBlinking = false;
+
+    void Start()
+    {
+        ImageToToggle.enabled = DefaultState;
+        TextToToggle.enabled = DefaultState;
+        StartBlink();
+    }
+
+    public void StartBlink()
+    {
+        // do not invoke the blink twice - needed if you need to start the blink from an external object
+        if (_isBlinking)
+            return;
+
+        if (ImageToToggle != null && TextToToggle != null)
+        {
+            _isBlinking = true;
+            InvokeRepeating("ToggleState", StartDelay, Interval);
+        }
+    }
+
+    public void ToggleState()
+    {
+        ImageToToggle.enabled = !ImageToToggle.enabled;
+        TextToToggle.enabled = !TextToToggle.enabled;
+    }
+
+}
